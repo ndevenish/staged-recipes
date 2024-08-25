4f0dea94e183e8eb055f21a9e543da1f560d2cc5
Nicholas Devenish <ndevenish@gmail.com>
2024-08-25 16:26:47 +0100

    Copy CMake from recipe

--- recipes/cpp-base64/build.sh
+++ recipes/cpp-base64/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cp ${RECIPE_DIR}/{CMakeLists.txt,Config.cmake.in} .
+
 mkdir -p build
 cd build
+
 cmake -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
